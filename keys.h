25c479b0f6070316f6ced32fd15472a9a35c4183
Lucas Rezzaghi <lbrezzaghi@gmail.com>
2021-11-21 10:35:37 -0300

    Update keys.h

--- keys.h
+++ keys.h
@@ -4,7 +4,7 @@
 #include <QString>
 
 
-const QString clientId = "570b07a49d774d33a1ff5254f5d97bfa";
-const QString clientSecret = "5ce3f75328c84be9a13d0a665479a273";
+const QString clientId = "";
+const QString clientSecret = "";
 
-#endif // SECRETKEYS_H
+#endif // KEYS_H
